fca425f9762f1038742ffac40d425eec7b1e6576
NgomsoGaya <ngomso.gaya09@gmail.com>
2023-08-04 15:55:56 +0200

    styling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import exphbs from "express-handlebars";
-import Greet from "./functions/factory";
+import Greet from "./functions/factory.js";
 
 const handlebarSetup = exphbs.engine({
     partialsDir: "./views/partials",
@@ -22,3 +22,21 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: "main" }));
 app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+    res.render("index", {
+        counter: greet.getCounter()
+    })
+});
+
+app.post('/settings', function (req, res) {
+
+    res.redirect('/')
+});
+
+
+
+const PORT = process.env.PORT || 3012
+
+app.listen(PORT, function () {
+    console.log("App started at port", PORT)
+});
